b3593f180deac1a7bd76c98bc4cc1997db9ecd56
edson angel <edsonangelmc@gmail.com>
2024-04-16 18:08:37 -0600

    models have to be improved, they should be changed from utils to controllers and controllers have to be modified as models, I will keep improving code

--- models/user-model.php
+++ models/user-model.php
@@ -1,30 +1,3 @@
 <?php
 
-    class User {
-        public $fname;
-        public $lname;
-        public $email;
-        public $password;
-        public $role;
-
-        function __construct($fname, $lname, $email, $password, $role) {
-            $this->fname = $fname;
-            $this->lname = $lname;
-            $this->email = $email;
-            $this->password = $password;      
-            $this->role = $role;  
-        }
-
-        function create_user() {
-
-        }
-
-        function update_user() {
-            
-        }
-
-        function delete_user() {
-            
-        }
-    }
 ?>
